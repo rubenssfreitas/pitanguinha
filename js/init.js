e41dc2ca58d30fcf061ad67f1360e3e31f49397f
Rubens Freitas <rubens.suporte@gmail.com>
2022-04-26 16:47:38 -0300

    V-13

--- js/init.js
+++ js/init.js
@@ -1,6 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+});
+
+// Or with jQuery
 
 $(document).ready(function () {
-    $('.parallax').parallax();
     $('.sidenav').sidenav();
+    $('.parallax').parallax();
 });
 
