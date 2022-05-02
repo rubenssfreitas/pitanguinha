913f544916e8ad76d06d7bf121655279976b471d
Rubens Freitas <rubens.suporte@gmail.com>
2022-05-02 17:58:24 -0300

    V-16

--- js/init.js
+++ js/init.js
@@ -11,3 +11,14 @@ $(document).ready(function () {
     $('.collapsible').collapsible();
 });
 
+var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
